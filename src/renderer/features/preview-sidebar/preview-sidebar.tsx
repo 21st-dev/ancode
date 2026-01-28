888ff6f956c9aeffd2c87aebcbeb62c422d91528
Nils <104727469+nilskroe@users.noreply.github.com>
2026-01-28 20:19:51 +0100

    Fix missing useSetAtom import in preview-sidebar

--- src/renderer/features/preview-sidebar/preview-sidebar.tsx
+++ src/renderer/features/preview-sidebar/preview-sidebar.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { atom, useAtom } from "jotai"
+import { atom, useAtom, useSetAtom } from "jotai"
 import { atomWithStorage, atomFamily } from "jotai/utils"
 import {
   Play,
